512844a0ed78f94a2067fed3e9e96485d1af79c1
Kaonuri <hon454@naver.com>
2017-11-28 04:09:53 +0900

    youtube play test success

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public string url;
+
+    UniWebView webView;
+
+    void Start()
+    {
+        var webViewGameObject = new GameObject("UniWebView");
+        webView = webViewGameObject.AddComponent<UniWebView>();
+
+        webView.Frame = new Rect(0, 0, Screen.width, Screen.height/2f);        
+        url = UniWebViewHelper.StreamingAssetURLForPath("local_www/" + url);
+        webView.SetVerticalScrollBarEnabled(false);
+        webView.Load(url);
+        webView.Show();
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public UniWebView webview;
+
+    private void Start()
+    {
+        var url = UniWebViewHelper.StreamingAssetURLForPath("local_www/test.html");
+        webview.Load(url);
+        webview.Show();
+    }
+}
